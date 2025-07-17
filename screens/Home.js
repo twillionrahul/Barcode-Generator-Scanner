38b3965d267c87cf5af4ea3bbd33440eeae906e3
Jatinder Kumar <jatinder@gettr.com>
2025-07-17 11:08:17 +0530

    Update barcode scanner app with UI fixes and permissions

--- screens/Home.js
+++ screens/Home.js
@@ -1,31 +1,31 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import { Button } from '@rneui/themed';
-import styles from './../styles/Style'
+import {View} from 'react-native';
+import {Button} from '@rneui/themed';
+import styles from './../styles/Style';
 
-function Home({ navigation }) {
-    return (
+function Home({navigation}) {
+  return (
     <View style={styles.container}>
-        <Button
+      <Button
         title="Generate"
         onPress={() => navigation.navigate('Barcode Generator')}
-        icon={{ ...styles.iconButtonHome, name: 'barcode' }}
+        icon={{...styles.iconButtonHome, name: 'barcode'}}
         iconContainerStyle={styles.iconButtonHomeContainer}
         titleStyle={styles.titleButtonHome}
         buttonStyle={styles.buttonHome}
         containerStyle={styles.buttonHomeContainer}
-        />
-        <Button
+      />
+      <Button
         title="Scanner"
         onPress={() => navigation.navigate('Barcode Scanner')}
-        icon={{ ...styles.iconButtonHome, name: 'barcode-scan' }}
+        icon={{...styles.iconButtonHome, name: 'barcode-scan'}}
         iconContainerStyle={styles.iconButtonHomeContainer}
         titleStyle={styles.titleButtonHome}
         buttonStyle={styles.buttonHome}
         containerStyle={styles.buttonHomeContainer}
-        />
+      />
     </View>
-    );
+  );
 }
 
-export default Home
+export default Home;
